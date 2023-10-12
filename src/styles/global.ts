8c5f7deb0ec422105b691f81c3b835c2d01de74d
matheus <de.leite2014@gmail.com>
2023-10-12 16:06:13 -0300

    feat: Adds global.css with global styles and ThemeProvider
    In this commit, a global.css file was implemented that uses the Styled Components createGlobalStyle function to define global styles for the project. Additionally, we introduced the theme object for managing custom CSS variables, which will be used throughout the project.
    
    Details:
    - Add global.css to the Styles folder for global project styles.
    - Uses createGlobalStyle from Styled Components to define global styles.
    - Creates an object called theme in the styles folder for managing custom CSS variables.
    - Implements the ThemeProvider in the layout to provide the theme object to all components.

---
+++ src/styles/global.ts
@@ -0,0 +1,39 @@
+import { createGlobalStyle } from "styled-components";
+import {theme} from "./theme";
+
+
+const GlobalStyles = createGlobalStyle`
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  font: inherit;
+}
+
+img {
+  display: block;
+  max-width: 100%;
+}
+
+ul {
+  list-style: none;
+}
+
+a {
+  text-decoration: none;
+}
+
+body {
+  min-height: 100vh;
+  font-family: ${theme.font.family.inter};
+}
+
+
+`
+
+export default GlobalStyles;
---
+++ src/styles/theme.ts
@@ -0,0 +1,43 @@
+import {Inter} from "next/font/google"
+
+const inter = Inter({
+  weight: ["400", "500", "600"],
+  subsets: ["latin"],
+});
+
+
+export const theme = {
+  border: {
+    radius: '5rem'
+  },
+  font: {
+    family: {
+      inter: inter.style.fontFamily
+    },
+    normal: 400,
+    medium: 500,
+    bold: 600,
+    size: {
+      xxsmall: '0.75rem',
+      xsmall: '0.9375rem',
+      small: '1rem',
+      medium: '2rem',
+      large: '2.75rem',
+    }
+  },
+  colors: {
+    black: "#130C25",
+    white: "#FFFFFF",
+    grey: "#423D51",
+    purple: "#5F41D9"
+  },
+  spacings: {
+    xxxsmall: '0.75rem',
+    xxsmall: '1.5rem',
+    xsmall: '2rem',
+    small: '2.5rem',
+    medium: '2.75rem',
+    large: '6.688rem',
+    xlarge: '9.5rem',
+  },
+}
