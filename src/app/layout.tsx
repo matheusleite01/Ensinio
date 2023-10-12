774e3e3a5295d26a564f3c096f238588eabfb0de
matheus <de.leite2014@gmail.com>
2023-10-12 15:43:00 -0300

    feat: Add Styled component for the project
    Added styled component for project styling. Following the next documentation, the following installation steps were performed:
    
    - Created "registry.tsx" in "lib" folder to create a global registry component to collect all CSS style rules.
    - Then used "StyledComponentsRegistry" to wrap the root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,24 @@
+'use client'
+import StyledComponentsRegistry from "@/lib/registry";
+import {ThemeProvider} from "styled-components"
+import { theme } from "@/styles/theme";
+import GlobalStyles from "@/styles/global";
+
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <ThemeProvider theme={theme}>
+          <StyledComponentsRegistry>
+            <GlobalStyles/>
+            {children}
+            </StyledComponentsRegistry>
+        </ThemeProvider>
+      </body>
     </html>
-  )
+  );
 }
