bca968b6838444565bffb8d9df3b9bacaece9e18
matheus <de.leite2014@gmail.com>
2023-10-14 18:18:24 -0300

    feat: add hero section on page
    Added to Hero section on the home page. Responsiveness for the section was also created.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,47 @@
-import React from 'react'
+"use client";
+import HeroTitle from "@/components/HeroTitle";
+import DevicesMobileIcon from "../../public/assets/icons/DevicesMobile-icon";
+import PlayIcon from "../../public/assets/icons/Play-icon";
+import * as S from "./styled";
+import Button from "@/components/Button";
+import Image from "next/image";
+import picture from "../../public/img/menImg.png";
+import imgBg from "../../public/assets/icons/Icons-bg.svg";
+
+const HeroOptions = {
+  subText: "plataforma all in one",
+  title: "Sua escola online poderosa e lucrativa",
+  text: "Tenha sua própria escola online 100% white label com rede social, gamificação, clube de assinaturas, ecommerce e sistema EAD completo.",
+  icon: <DevicesMobileIcon />,
+};
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <S.HeroContainer>
+        <div className="container">
+          <S.ImgBg>
+            <Image src={imgBg} alt="imgBg" />
+          </S.ImgBg>
+          <S.HeroSection>
+            <div>
+              <HeroTitle HeroOptions={HeroOptions} />
+              <S.ButtonContainer>
+                <Button backgroundColor="#00E1E7" color="#130C25" border="none">
+                  Começar agora
+                </Button>
+                <S.spanVideo>
+                  <PlayIcon />
+                  Ver vídeo
+                </S.spanVideo>
+              </S.ButtonContainer>
+            </div>
+            <Image src={picture} alt="men" style={{ objectFit: "contain" }} />
+          </S.HeroSection>
+        </div>
+      </S.HeroContainer>
+    </>
+  );
+};
 
-export default Home
+export default Home;
---
+++ src/app/styled.ts
@@ -0,0 +1,53 @@
+import styled from "styled-components";
+import { theme } from "@/styles/theme";
+
+export const HeroContainer = styled.section`
+  padding: 10rem 2rem 0 2rem;
+  background: linear-gradient(85deg, #5f41d9 2.25%, #41b5d9 100.27%);
+`;
+
+export const ImgBg = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  @media screen and (max-width: 1200px) {
+    display: none;
+  }
+`;
+
+export const HeroSection = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  @media screen and (max-width: 1066px) {
+    align-items: center;
+    flex-direction: column;
+    gap: ${theme.spacings.medium};
+  }
+`;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  gap: ${theme.spacings.medium};
+  margin-top: ${theme.spacings.medium};
+  button {
+    font-size: ${theme.font.size.small};
+    padding: 18px 32px;
+  }
+  @media screen and (max-width: 1066px) {
+    justify-content: center;
+  }
+`;
+
+export const spanVideo = styled.span`
+  display: flex;
+  align-items: center;
+  color: ${theme.colors.white};
+  gap: ${theme.spacings.xxxxsmall};
+  cursor: pointer;
+  transition: all 0.2s;
+
+  &:hover {
+    transform: translateY(3px);
+  }
+`;
